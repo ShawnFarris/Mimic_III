c42e0778b01007065a7576fc1100179ca799f292
Shawn <shawnfarris7@gmail.com>
2017-10-30 15:29:17 -0400

    _

--- data_explorations.py
+++ data_explorations.py
@@ -24,12 +24,13 @@ sqluser = 'postgres'
 dbname = 'mimic'
 schema_name = 'mimiciii'
 
-
 # Specify List of Cookbook Queries from MIMIC Repo
 query_path = '/Users/ShawnFarris/mimic-code/concepts/cookbook'
 query_list = glob.glob(query_path + "/*.sql")
 query_names = [query_list[i].split("/")[-1] for i in range(len(query_list))]
 
+t = query_list[0].split("/")[-1]
+t.split(".sql")[0]
 
 
 # Connect to local postgres version of mimic
@@ -44,12 +45,39 @@ def get_query(query_name):
         query = fp.read()
     return query
 
-
+#### Age-Histogram
 query = get_query('age-histogram')
+df = pd.read_sql_query(query,con)
+df['age'].plot(kind = 'hist', logy = True)
+df['age'].plot(kind = 'kde', logy = True)
 
-model_df = pd.read_sql_query(query,con)
+#### Basic_Patient_Info
+query = get_query(query_names[1].split(".sql")[0])
+df = pd.read_sql_query(query,con)
+df.head()
 
-model_df
+#### ICD9 Count -- for Hyper
+query = get_query('icd9count')
+df = pd.read_sql(query,con)
+df
+
+query = 'icd9vicd9count'
+df = pd.read_sql(query,con)
+df
+
+query = '''
+select icd9_code, count(*) as cnt 
+from diagnoses_icd 
+group by icd9_code 
+order by cnt desc;'''
+df = pd.read_sql(query,con)
+df
+
+query = '''
+select *
+from diagnoses_icd
+'''
+
 
 
 
