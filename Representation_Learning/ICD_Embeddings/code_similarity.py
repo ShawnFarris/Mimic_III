5dbb77daff1ab69f3449324bfa3bf55f938d04e4
ShawnFarris <shawnfarris7@gmail.com>
2018-05-04 10:33:12 -0400

    updated code_similarity.py

--- Representation_Learning/ICD_Embeddings/code_similarity.py
+++ Representation_Learning/ICD_Embeddings/code_similarity.py
@@ -12,15 +12,18 @@ import argparse
 def get_args():
     
     parser = argparse.ArgumentParser()
-    parser.add_argument("-code","--code", dest = 'code', type = str, action = 'store') 
-    parser.set_defaults(code = '401')
+    parser.add_argument("--show_distance", dest = 'show_show_distance', action = 'store_true')
+    parser.add_argument("--show_desc", dest = 'show_show_desc', action = 'store_true')
+    parser.set_defaults(show_distance = False, show_desc = False)
     args = parser.parse_args()    
     
     return args
 
 args = get_args()
 print(args)
+
 base = os.getcwd()
+pd.set_option('display.max_colwidth', 100)
 
 #### Load ICD decriptions
 icd9_desc = pd.read_csv(base + '/ICD_Data/icd9_desc.csv')
@@ -28,12 +31,8 @@ icd9_desc = pd.read_csv(base + '/ICD_Data/icd9_desc.csv')
 #### Script Parameters
 np.random.seed(12)
 
-#### Load tsne data
-df_tsne = pd.read_csv(base + '/Processed_Data/icd_tsne.csv')
-
-
 #### Load embedding dict
-with open(base + '/icd_embedding_dict.pickle', 'rb') as handle:
+with open(base + '/Processed_Data/icd_embedding_dict.pickle', 'rb') as handle:
    embedding_dict = pickle.load(handle)
 
 X = embedding_dict.get('embedding_matrix')
@@ -47,11 +46,61 @@ def get_top_codes(code):
     top_words.rename(columns = {0:'icd9',1:'Distance'},inplace=True)
     top_words = pd.merge(top_words,icd9_desc,how='inner')
     
+    if args.show_distance == False:
+        top_words.drop('Distance',axis=1,inplace=True)
+        
+    if args.show_desc == False:
+        top_words.drop('description',axis=1,inplace=True)
+    pd.options.display.max_colwidth          
     target_word_desc = icd9_desc[icd9_desc['icd9'] == code]['description'].values[0]
+    print('\n')
     print('Most similar codes for ' + code + ' (' + target_word_desc + '): ')
-    print(top_words)
+    print('\n')
+    print(top_words.to_string())
+    
 
 
-get_top_codes(args.code)
+#options.display.max_colwidth
+
+#### check if folder_path already saved
+# To Do: eliminate need to rerun script
+def get_code_similarity():
+    stop_script = False
+    while stop_script == False:        
+        try:
+            code = input("Input code or  'stop' to close script: ")
+            if code[0] == code[-1] == "'" or code[0] == code[-1] == '"':
+                code = code[1:-1]
+           
+            if code =='stop':
+                stop_script = True
+            else:
+                get_top_codes(code)
+                
+        except pd.errors.MergeError:
+            print('Invalid code.')
+
+get_code_similarity()      
+
+      
+#        except FileNotFoundError:
+#            mimic_data_path = input("Path to Mimic data not found. Enter path, unqouted: ")
+#            if mimic_data_path[0] == mimic_data_path[-1] == "'" or mimic_data_path[0] == mimic_data_path[-1] == '"':
+#                mimic_data_path = mimic_data_path[1:-1]
+#                
+#            folder_paths = {'mimic_data_path':mimic_data_path}
+#            # Save in Seperate JSON
+#            with open(os.getcwd() + '/folder_paths.json','w') as fp:
+#                json.dump(folder_paths, fp)
+#        
+
+
+
+
+
+
+
+
+#get_top_codes(args.code)
 
 
